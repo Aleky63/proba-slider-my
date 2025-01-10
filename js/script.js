b54b500db8e2fe62a00e7ef5a6525d7f986680d7
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-01-10 13:58:35 +0300

    01_10.01

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new swiper(".swiper", {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 5,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
